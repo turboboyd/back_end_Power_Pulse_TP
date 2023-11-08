ac291e8c3f6c7064cd02691ac8aa62e997a6c686
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-11-09 00:52:00 +0100

    add

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
