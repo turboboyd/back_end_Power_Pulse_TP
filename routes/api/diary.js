322771e4bf84fbbe803ae9af6221ec3586f37fd9
Oleksandr <live.gp@gmail.com>
2023-11-12 10:22:51 +0200

    fix diary

--- routes/api/diary.js
+++ routes/api/diary.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
+import ctrlDiary from "../../controllers/diary.js";
 import ctrlDiaryProducts from "../../controllers/diary-products.js";
 import ctrlDiaryExercises from "../../controllers/diary-exercises.js";
 import authenticate from '../../middlewares/authenticate.js';
 
 const router = Router();
 
-// router.get("/", authenticate, ctrlDiary.listDiary);
+router.get("/", authenticate, ctrlDiary.listDiary);
 
 router.get("/products", authenticate, ctrlDiaryProducts.listDiaryProducts);
 router.post("/products", authenticate, ctrlDiaryProducts.addDiaryProducts);
