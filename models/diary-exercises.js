7f467799c4d1a0655a6ff7ce35d8cbc051059c3d
Oleksandr <live.gp@gmail.com>
2023-11-12 16:55:09 +0200

    add validate

--- models/diary-exercises.js
+++ models/diary-exercises.js
@@ -1,4 +1,7 @@
 import { Schema, model } from "mongoose";
+import Joi from "joi";
+
+import { Exercise } from "./exercises.js";
 
 const diaryExerciseSchema = new Schema(
   {
@@ -33,5 +36,60 @@ const diaryExerciseSchema = new Schema(
   },
   { versionKey: false }
 );
+
+const addSchema = Joi.object({
+  date: Joi.date().required().messages({
+    "date.base": "date must be a valid date",
+    "any.required": "date is a required field"
+  }),
+  exercises: Joi.array()
+    .items(
+      Joi.object({
+        exercise: Joi.string()
+          .required()
+          .hex()
+          .length(24)
+          .external(async (value) => {
+            const exercise = await Exercise.findById(value);
+            if (!exercise) {
+              throw new Error("exercise does not exist");
+            }
+            return value;
+          })
+          .messages({
+            "string.base": "exercise must be a valid ObjectId",
+            "string.hex": "exercise must be a valid ObjectId",
+            "string.length": "exercise must be a valid ObjectId",
+            "any.required": "exercise is a required field"
+          }),
+        time: Joi.number()
+          .required()
+          .min(1)
+          .messages({
+            "number.base": "time must be a positive number",
+            "number.min": "time must be a positive number",
+            "any.required": "time is a required field"
+          }),
+        calories: Joi.number()
+          .required()
+          .min(1)
+          .messages({
+            "number.base": "calories must be a positive number",
+            "number.min": "calories must be a positive number",
+            "any.required": "calories is a required field"
+          }),
+      })
+    )
+    .min(1)
+    .messages({
+      "array.base": "exercises must be a non-empty array",
+      "array.min": "exercises must be a non-empty array"
+    }),
+});
+
+
+export const schemasDiaryExercise = {
+  addSchema,
+};
 
 export const DiaryExercise = model('diary-exercise', diaryExerciseSchema);
--- models/diary-products.js
+++ models/diary-products.js
@@ -1,4 +1,7 @@
 import { Schema, model } from "mongoose";
+import Joi from "joi";
+
+import { Product } from "./products.js";
 
 const diaryProductSchema = new Schema(
   {
@@ -28,10 +31,65 @@ const diaryProductSchema = new Schema(
           min: 1,
           required: true,
         },
-      }
+      },
     ],
   },
   { versionKey: false }
 );
+
+const addSchema = Joi.object({
+  date: Joi.date().required().messages({
+    "date.base": "date must be a valid date",
+    "any.required": "date is a required field"
+  }),
+  products: Joi.array()
+    .items(
+      Joi.object({
+        product: Joi.string()
+          .required()
+          .hex()
+          .length(24)
+          .external(async (value) => {
+            const product = await Product.findById(value);
+            if (!product) {
+              throw new Error("product does not exist");
+            }
+            return value;
+          })
+          .messages({
+            "string.base": "product must be a valid ObjectId",
+            "string.hex": "product must be a valid ObjectId",
+            "string.length": "product must be a valid ObjectId",
+            "any.required": "product is a required field"
+          }),
+        amount: Joi.number()
+          .required()
+          .min(1)
+          .messages({
+            "number.base": "amount must be a positive number",
+            "number.min": "amount must be a positive number",
+            "any.required": "amount is a required field"
+          }),
+        calories: Joi.number()
+          .required()
+          .min(1)
+          .messages({
+            "number.base": "calories must be a positive number",
+            "number.min": "calories must be a positive number",
+            "any.required": "calories is a required field"
+          }),
+      })
+    )
+    .min(1)
+    .messages({
+      "array.base": "products must be a non-empty array",
+      "array.min": "products must be a non-empty array"
+    }),
+});
+
+
+export const schemasDiaryProducts = {
+  addSchema,
+};
 
 export const DiaryProduct = model("diary-product", diaryProductSchema);
