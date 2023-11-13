2bba66590f49166ea7e34fb4d739bb839b0a595e
Oleksandr <live.gp@gmail.com>
2023-11-13 20:28:27 +0200

    Update validateBody.js

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -1,19 +1,3 @@
-// import httpError from "./HttpError.js";
-
-// const validateBody = schema => {
-//     const func = (req, res, next) => {
-//         const { error } = schema.validate(req.body);
-//         if (error) {
-//             return next(httpError(400, error.message));
-//         }
-//         next()
-//     }
-
-//     return func;
-// }
-
-// export default validateBody;
-
 import httpError from "./HttpError.js";
 
 const validateBody = (schema) => {
