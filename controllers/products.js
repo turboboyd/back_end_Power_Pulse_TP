e008ffdc88cf1f035990d12b6dcaab5d3b96f950
Oleksandr <live.gp@gmail.com>
2023-11-13 21:24:05 +0200

    Update products.js
    upd ctrl

--- controllers/products.js
+++ controllers/products.js
@@ -3,23 +3,25 @@ import { ProfileSettings } from "../models/profile-settings-model.js";
 import ctrlWrapper from "../helpers/ctrlWrapper.js";
 
 const listProducts = async (req, res) => {
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, search, category, recommended } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Product.find({}, null, { skip, limit });
-  res.json(result);
-};
-
-const listProductsBlood = async (req, res) => {
   const { _id: owner } = req.user;
-  const {allowBloodType} = req.query;
-  const { blood } = await ProfileSettings.findOne({ owner });
-  const key = `groupBloodNotAllowed.${blood}`;
-  const result = await Product.find({ [key]: allowBloodType });
+  let query;
+  if (search) {
+    query = { title: { $regex: search, $options: "i" } };
+  } else if (category) {
+    query = { category: { $regex: category, $options: "i" } };
+  } else if (recommended) {
+    const { blood } = await ProfileSettings.findOne({ owner });
+    const key = `groupBloodNotAllowed.${blood}`;
+    query = { [key]: recommended };
+  } else {
+    query = {};
+  }
+  const result = await Product.find(query, null, { skip, limit });
   res.json(result);
 };
-
 
 export default {
   listProducts: ctrlWrapper(listProducts),
-  listProductsBlood: ctrlWrapper(listProductsBlood),
 };
