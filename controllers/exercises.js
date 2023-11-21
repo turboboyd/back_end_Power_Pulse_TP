b36e07a6c83feb58b10c965760d6f4e38b57278d
Oleksandr <live.gp@gmail.com>
2023-11-21 20:07:40 +0200

    Update exercises.js

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -13,14 +13,24 @@ const listExercisesTypes = async (req, res) => {
 
 const listExercises = async (req, res) => {
   const { page = 1, limit = 20, id } = req.query;
-  const skip = (page - 1) * limit;
   const { name } = await ExerciseСategory.findById(id);
-  const query = ({ bodyPart: { $regex: name, $options: "i" } });
+  const categoryRegex = { $regex: name, $options: "i" };
+  const query = {
+    $or: [
+      { bodyPart: categoryRegex },
+      { equipment: categoryRegex },
+      { name: categoryRegex },
+      { target: categoryRegex },
+    ],
+  };
+
+  const skip = (page - 1) * limit;
   const totalRecords = await Exercise.countDocuments(query);
   const result = await Exercise.find(query, null, { skip, limit });
   res.setHeader('X-Total-Count', totalRecords);
   res.json(result);
 };
+
 
 export default {
   listExercises: ctrlWrapper(listExercises),
